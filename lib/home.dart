7b5666f649d7472517e579f5bd16b0c75799a499
dannyoo7 <dannylanssink@outloo.com>
2020-06-03 14:00:13 +0200

    routing toegevoegd (main.dart) backend - corona_class verschillende pagina,s aan gemaakt

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:moa_corona_examen_applicatie/choose_location.dart';
 import 'package:moa_corona_examen_applicatie/corona_class.dart';
+import 'package:moa_corona_examen_applicatie/corona_rules.dart';
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -9,6 +12,26 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Text('corona regels', style: TextStyle(color: Colors.white),),
+                onPressed: (){Navigator.push(context,MaterialPageRoute(builder: (context) => Coronarules()));}, //navigeert naar corona_rules.dart
+              ),
+            ),
+            Expanded(
+            child: FlatButton(
+              child: Text('ander land', style: TextStyle(color: Colors.white),),
+              onPressed: (){Navigator.push(context,MaterialPageRoute(builder: (context) => ChooseLocation()));}, //navigeert naar choose_location.dart
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
